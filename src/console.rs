b08a095de8d6447a33ab660929e7982c32a63876
dmrasf <dmrasf@qq.com>
2022-12-01 10:37:35 +0800

    彩色输出

--- src/console.rs
+++ src/console.rs
@@ -1,7 +1,21 @@
+#![allow(unused)]
+
 use crate::sbi::console_putchar;
 use core::fmt::{self, Write};
 
 struct Stdout;
+
+#[derive(PartialEq, PartialOrd)]
+pub enum LOG {
+    DISABLE = -1,
+    ERROR = 0,
+    WARN = 1,
+    INFO = 2,
+    DEBUG = 3,
+    TRACE = 4,
+}
+
+pub const LOG_LEVEL: LOG = LOG::TRACE;
 
 impl Write for Stdout {
     fn write_str(&mut self, s: &str) -> fmt::Result {
@@ -27,5 +41,47 @@ macro_rules! print {
 macro_rules! println {
     ($fmt: literal $(, $($arg: tt)+)?) => {
         $crate::console::print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! error {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        if $crate::console::LOG_LEVEL >= $crate::console::LOG::ERROR {
+            $crate::console::print(format_args!(concat!("\x1b[1;31m[ERROR]\x1b[0m\x1b[31m ", $fmt, "\x1b[0m\n") $(, $($arg)+)?));
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! warn {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        if $crate::console::LOG_LEVEL >= $crate::console::LOG::WARN {
+            $crate::console::print(format_args!(concat!("\x1b[1;93m[WARN]\x1b[0m\x1b[93m ", $fmt, "\x1b[0m\n") $(, $($arg)+)?));
+        }
+    }
+}
+#[macro_export]
+macro_rules! info {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        if $crate::console::LOG_LEVEL >= $crate::console::LOG::INFO {
+            $crate::console::print(format_args!(concat!("\x1b[1;34m[INFO]\x1b[0m\x1b[34m ", $fmt, "\x1b[0m\n") $(, $($arg)+)?));
+        }
+    }
+}
+#[macro_export]
+macro_rules! debug {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        if $crate::console::LOG_LEVEL >= $crate::console::LOG::DEBUG {
+            $crate::console::print(format_args!(concat!("\x1b[1;32m[DEBUG]\x1b[0m\x1b[32m ", $fmt, "\x1b[0m\n") $(, $($arg)+)?));
+        }
+    }
+}
+#[macro_export]
+macro_rules! trace {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        if $crate::console::LOG_LEVEL >= $crate::console::LOG::TRACE {
+            $crate::console::print(format_args!(concat!("\x1b[1;90m[TRACE]\x1b[0m\x1b[90m ", $fmt, "\x1b[0m\n") $(, $($arg)+)?));
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![allow(unused)]
 
 #[macro_use]
 mod console;
@@ -11,9 +12,29 @@ use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
-pub fn rust_main() -> ! {
+pub fn rust_main() {
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack_lower_bound(); // stack lower bound
+        fn boot_stack_top(); // stack top
+    }
     clear_bss();
-    loop {}
+    info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    info!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    info!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    info!(
+        "boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack_lower_bound as usize
+    );
+    info!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    sbi::shutdown();
 }
 
 fn clear_bss() {
