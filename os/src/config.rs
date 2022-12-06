31959221ec9836f50ba93f0bb4b9986b006e4307
dmrasf <dmrasf@qq.com>
2022-12-06 09:52:52 +0800

    动态内存分配

--- os/src/config.rs
+++ os/src/config.rs
@@ -4,5 +4,9 @@ pub const MAX_APP_NUM: usize = 4;
 pub const APP_BASE_ADDRESS: usize = 0x80400000;
 pub const APP_SIZE_LIMIT: usize = 0x20000;
 
-pub use crate::board::CLOCK_FREQ;
+pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
+pub const MEMORY_END: usize = 0x80800000;
+pub const PAGE_SIZE: usize = 0x1000;
+pub const PAGE_SIZE_BITS: usize = 0xc;
 
+pub use crate::board::CLOCK_FREQ;
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![allow(unused)]
+#![feature(alloc_error_handler)]
 
 #[macro_use]
 mod console;
@@ -8,6 +9,7 @@ mod board;
 mod config;
 mod lang_items;
 mod loader;
+mod mm;
 mod sbi;
 mod sync;
 mod syscall;
@@ -15,6 +17,7 @@ mod task;
 mod timer;
 mod trap;
 
+extern crate alloc;
 use core::arch::global_asm;
 
 global_asm!(include_str!("entry.asm"));
@@ -23,6 +26,8 @@ global_asm!(include_str!("link_app.S"));
 #[no_mangle]
 pub fn rust_main() {
     clear_bss();
+    mm::init_heap();
+    mm::heap_test();
     trap::init();
     loader::load_apps();
     trap::enable_timer_interrupt();
