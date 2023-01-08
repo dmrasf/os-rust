b11e70eb9d1cfeaf90974258ab95d888de5f3066
dmrasf <dmrasf@qq.com>
2023-01-08 21:18:10 +0800

    ch8-user

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -132,120 +132,89 @@ pub fn waitpid_nb(pid: usize, exit_code: &mut i32) -> isize {
     sys_waitpid(pid as isize, exit_code as *mut _)
 }
 
-pub fn sleep(period_ms: usize) {
-    let start = sys_get_time();
-    while sys_get_time() < start + period_ms as isize {
-        sys_yield();
+bitflags! {
+    pub struct SignalFlags: i32 {
+        const SIGINT    = 1 << 2;
+        const SIGILL    = 1 << 4;
+        const SIGABRT   = 1 << 6;
+        const SIGFPE    = 1 << 8;
+        const SIGSEGV   = 1 << 11;
     }
 }
 
-/// Action for a signal
-#[repr(C, align(16))]
-#[derive(Debug, Clone, Copy)]
-pub struct SignalAction {
-    pub handler: usize,
-    pub mask: SignalFlags,
+pub fn kill(pid: usize, signal: i32) -> isize {
+    sys_kill(pid, signal)
 }
 
-impl Default for SignalAction {
-    fn default() -> Self {
-        Self {
-            handler: 0,
-            mask: SignalFlags::empty(),
+pub fn sleep(sleep_ms: usize) {
+    sys_sleep(sleep_ms);
+}
+
+pub fn thread_create(entry: usize, arg: usize) -> isize {
+    sys_thread_create(entry, arg)
+}
+pub fn gettid() -> isize {
+    sys_gettid()
+}
+pub fn waittid(tid: usize) -> isize {
+    loop {
+        match sys_waittid(tid) {
+            -2 => {
+                yield_();
+            }
+            exit_code => return exit_code,
         }
     }
 }
 
-pub const SIGDEF: i32 = 0; // Default signal handling
-pub const SIGHUP: i32 = 1;
-pub const SIGINT: i32 = 2;
-pub const SIGQUIT: i32 = 3;
-pub const SIGILL: i32 = 4;
-pub const SIGTRAP: i32 = 5;
-pub const SIGABRT: i32 = 6;
-pub const SIGBUS: i32 = 7;
-pub const SIGFPE: i32 = 8;
-pub const SIGKILL: i32 = 9;
-pub const SIGUSR1: i32 = 10;
-pub const SIGSEGV: i32 = 11;
-pub const SIGUSR2: i32 = 12;
-pub const SIGPIPE: i32 = 13;
-pub const SIGALRM: i32 = 14;
-pub const SIGTERM: i32 = 15;
-pub const SIGSTKFLT: i32 = 16;
-pub const SIGCHLD: i32 = 17;
-pub const SIGCONT: i32 = 18;
-pub const SIGSTOP: i32 = 19;
-pub const SIGTSTP: i32 = 20;
-pub const SIGTTIN: i32 = 21;
-pub const SIGTTOU: i32 = 22;
-pub const SIGURG: i32 = 23;
-pub const SIGXCPU: i32 = 24;
-pub const SIGXFSZ: i32 = 25;
-pub const SIGVTALRM: i32 = 26;
-pub const SIGPROF: i32 = 27;
-pub const SIGWINCH: i32 = 28;
-pub const SIGIO: i32 = 29;
-pub const SIGPWR: i32 = 30;
-pub const SIGSYS: i32 = 31;
-
-bitflags! {
-    pub struct SignalFlags: i32 {
-        const SIGDEF = 1; // Default signal handling
-        const SIGHUP = 1 << 1;
-        const SIGINT = 1 << 2;
-        const SIGQUIT = 1 << 3;
-        const SIGILL = 1 << 4;
-        const SIGTRAP = 1 << 5;
-        const SIGABRT = 1 << 6;
-        const SIGBUS = 1 << 7;
-        const SIGFPE = 1 << 8;
-        const SIGKILL = 1 << 9;
-        const SIGUSR1 = 1 << 10;
-        const SIGSEGV = 1 << 11;
-        const SIGUSR2 = 1 << 12;
-        const SIGPIPE = 1 << 13;
-        const SIGALRM = 1 << 14;
-        const SIGTERM = 1 << 15;
-        const SIGSTKFLT = 1 << 16;
-        const SIGCHLD = 1 << 17;
-        const SIGCONT = 1 << 18;
-        const SIGSTOP = 1 << 19;
-        const SIGTSTP = 1 << 20;
-        const SIGTTIN = 1 << 21;
-        const SIGTTOU = 1 << 22;
-        const SIGURG = 1 << 23;
-        const SIGXCPU = 1 << 24;
-        const SIGXFSZ = 1 << 25;
-        const SIGVTALRM = 1 << 26;
-        const SIGPROF = 1 << 27;
-        const SIGWINCH = 1 << 28;
-        const SIGIO = 1 << 29;
-        const SIGPWR = 1 << 30;
-        const SIGSYS = 1 << 31;
-    }
+pub fn mutex_create() -> isize {
+    sys_mutex_create(false)
+}
+pub fn mutex_blocking_create() -> isize {
+    sys_mutex_create(true)
+}
+pub fn mutex_lock(mutex_id: usize) {
+    sys_mutex_lock(mutex_id);
+}
+pub fn mutex_unlock(mutex_id: usize) {
+    sys_mutex_unlock(mutex_id);
+}
+pub fn semaphore_create(res_count: usize) -> isize {
+    sys_semaphore_create(res_count)
+}
+pub fn semaphore_up(sem_id: usize) {
+    sys_semaphore_up(sem_id);
+}
+pub fn semaphore_down(sem_id: usize) {
+    sys_semaphore_down(sem_id);
+}
+pub fn condvar_create() -> isize {
+    sys_condvar_create(0)
+}
+pub fn condvar_signal(condvar_id: usize) {
+    sys_condvar_signal(condvar_id);
+}
+pub fn condvar_wait(condvar_id: usize, mutex_id: usize) {
+    sys_condvar_wait(condvar_id, mutex_id);
 }
 
-pub fn kill(pid: usize, signum: i32) -> isize {
-    sys_kill(pid, signum)
+#[macro_export]
+macro_rules! vstore {
+    ($var_ref: expr, $value: expr) => {
+        unsafe { core::intrinsics::volatile_store($var_ref as *const _ as _, $value) }
+    };
 }
 
-pub fn sigaction(
-    signum: i32,
-    action: Option<&SignalAction>,
-    old_action: Option<&mut SignalAction>,
-) -> isize {
-    sys_sigaction(
-        signum,
-        action.map_or(core::ptr::null(), |a| a),
-        old_action.map_or(core::ptr::null_mut(), |a| a)
-    )
+#[macro_export]
+macro_rules! vload {
+    ($var_ref: expr) => {
+        unsafe { core::intrinsics::volatile_load($var_ref as *const _ as _) }
+    };
 }
 
-pub fn sigprocmask(mask: u32) -> isize {
-    sys_sigprocmask(mask)
-}
-
-pub fn sigreturn() -> isize {
-    sys_sigreturn()
+#[macro_export]
+macro_rules! memory_fence {
+    () => {
+        core::sync::atomic::fence(core::sync::atomic::Ordering::SeqCst)
+    };
 }
